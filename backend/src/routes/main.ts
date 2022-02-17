ff8cc1da2b17084461231a6cc38838f1488ea4f7
JoseO1985 <ordicu85@gmail.com>
2022-02-17 23:40:42 +0000

    Connecting auth routes to main router

--- backend/src/routes/main.ts
+++ backend/src/routes/main.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import { authRouter } from './auth';
 
 export const mainRouter = Router();
 
 mainRouter.get('/', (_, res) => {
   res.json({ res: 'hello world' });
 });
+
+mainRouter.use('/api/auth', authRouter);
