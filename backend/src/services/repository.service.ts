51c350407dd625083a88c502a10032a4e1b94572
JoseO1985 <ordicu85@gmail.com>
2022-02-19 17:32:01 +0000

    Adding database repository service

---
+++ backend/src/services/repository.service.ts
@@ -0,0 +1,24 @@
+import { FilterQuery, PaginateModel, Document } from 'mongoose';
+import { Model } from 'mongoose';
+import { getPagination } from '../util/query';
+
+export const paginate = (
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  model: PaginateModel<any, {}, {}>,
+  page?: string,
+  size?: string,
+  select?: string | string[],
+  filter: FilterQuery<any> = {}
+) => {
+  const { limit, offset } = getPagination(page, size);
+  return model.paginate(filter, { offset, limit, select });
+};
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const getOne = (id: string, model: Model<any, {}, {}>) => {
+  return model.findOne({ _id: id });
+};
+
+export const findOne = (model: Model<any, {}, {}>, query: FilterQuery<Document>) => {
+  return model.findOne(query);
+};
