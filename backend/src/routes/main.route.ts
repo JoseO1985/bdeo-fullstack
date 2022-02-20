84ccaec5ab00cb29c39f3af81ed7a6efb451e705
JoseO1985 <ordicu85@gmail.com>
2022-02-20 01:33:04 +0000

    Connecting user routes to main router

--- backend/src/routes/main.route.ts
+++ backend/src/routes/main.route.ts
@@ -3,6 +3,7 @@ import { authRouter } from './auth.route';
 import { beerRouter } from './beer.route';
 import * as authMiddleware from '../middlewares/auth.middleware';
 import * as httpLoggerMiddleware from '../middlewares/http-logger.middleware';
+import { userRouter } from './user.route';
 
 export const mainRouter = Router();
 
@@ -14,3 +15,4 @@ mainRouter.get('/', (_, res) => {
 
 mainRouter.use('/api/auth', authRouter);
 mainRouter.use('/api/beers', authMiddleware.protect, beerRouter);
+mainRouter.use('/api/users', authMiddleware.protect, userRouter);
