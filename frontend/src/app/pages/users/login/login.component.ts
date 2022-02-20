8b8d5adfcc129986a941d4b935b79fc79f1238ee
JoseO1985 <ordicu85@gmail.com>
2022-02-20 23:53:34 +0000

    Adding login component

--- frontend/src/app/pages/users/login/login.component.ts
+++ frontend/src/app/pages/users/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+  loading = false;
+  returnUrl = "";
+
+  constructor(
+    private loginService: LoginService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private toastrService: ToastrService
+  ) { }
 
   ngOnInit(): void {
+    this.createForm();
+    this.loginService.logout();
+    this.returnUrl = this.route.snapshot.queryParams[`returnUrl`] || "/";
+  }
+
+  createForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+    });
+  }
+
+  login(): void {
+    if (this.loginForm.valid) {
+      this.loading = true;
+      this.loginService
+        .login(
+          this.loginForm.controls['email'].value,
+          this.loginForm.controls['password'].value
+        )
+        .subscribe(
+          data => {
+            this.loading = false;
+            this.router.navigate([this.returnUrl]);
+          },
+          ({error}) => {
+            this.toastrService.error(error.message);
+            this.loading = false;
+          }
+        );
+    } else {
+      this.toastrService.error("Invalid credentials");
+    }
   }
 
 }
