de84ad8e661f995a81ac9b5a217017a6ba81a962
JoseO1985 <ordicu85@gmail.com>
2022-02-20 23:58:20 +0000

    Refactoring user routing

--- frontend/src/app/pages/users/users-routing.module.ts
+++ frontend/src/app/pages/users/users-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from 'src/app/core/layout/layout.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent }
+  {
+    path: "",
+    component: LayoutComponent,
+    children: [
+      { path: "", redirectTo: '/beers', pathMatch: 'full' },
+      { path: "login", component: LoginComponent },
+      { path: "register", component: RegisterComponent }
+    ]
+  }
 ];
 
 @NgModule({
