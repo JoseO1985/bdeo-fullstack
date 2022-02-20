1d9c755a5c9de50388f7815a57e411f58a039bf7
JoseO1985 <ordicu85@gmail.com>
2022-02-20 23:59:41 +0000

    Adding jwt token to every requests

---
+++ frontend/src/app/core/helpers/jwt.interceptor.ts
@@ -0,0 +1,28 @@
+﻿import { Injectable } from "@angular/core";
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (typeof window !== "undefined") {
+      const currentUser = localStorage.getItem("currentUser");
+      const parsedUser = currentUser ? JSON.parse(currentUser) : null;
+      if (parsedUser && parsedUser.token) {
+        request = request.clone({
+          setHeaders: {
+            Authorization: `Bearer ${parsedUser.token}`
+          }
+        });
+      }
+    }
+
+    return next.handle(request);
+  }
+}
+
+export const JwtInterceptorProvider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: JwtInterceptor,
+  multi: true
+};
