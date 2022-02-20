89acf6e52870ccafd2e1fd6be29d1e521323bbf2
JoseO1985 <ordicu85@gmail.com>
2022-02-20 20:58:35 +0000

    Connecting module routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import("./pages/users/users.module").then((module) => module.UsersModule)
+  },
+  {
+    path: "beers",
+   // canActivate: [AuthGuard],
+    loadChildren: () => import("./pages/beers/beers.module").then((module) => module.BeersModule)
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
