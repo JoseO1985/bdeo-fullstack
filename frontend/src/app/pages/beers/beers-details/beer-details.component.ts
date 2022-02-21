1af8166f9d38f6c990a9c818f2d43f10bdebaa7c
JoseO1985 <ordicu85@gmail.com>
2022-02-21 17:08:48 +0000

    Reading beer id in beer details page

--- frontend/src/app/pages/beers/beers-details/beer-details.component.ts
+++ frontend/src/app/pages/beers/beers-details/beer-details.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-beer-details',
   templateUrl: './beer-details.component.html',
   styleUrls: ['./beer-details.component.scss']
 })
-export class BeerDetailsComponent implements OnInit {
+export class BeerDetailsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  beerId = "";
+  subscription!: Subscription;
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.subscription = this.route.paramMap.subscribe(params => {
+      this.beerId= params.get('id') || "";
+      console.log(this.beerId);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
