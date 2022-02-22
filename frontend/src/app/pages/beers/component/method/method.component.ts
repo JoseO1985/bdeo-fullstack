5802be4e0f8c7d27259385c667d4b1a8d3cfe3fe
JoseO1985 <ordicu85@gmail.com>
2022-02-22 00:54:14 +0000

    Adding method component

---
+++ frontend/src/app/pages/beers/component/method/method.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Method } from '../../models/method';
+
+@Component({
+  selector: 'app-method',
+  templateUrl: './method.component.html',
+  styleUrls: ['./method.component.scss']
+})
+export class MethodComponent implements OnInit {
+  @Input() method!: Method;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
