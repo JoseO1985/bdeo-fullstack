f2a141bb2654cf2bd447535b9e43be55e3317377
JoseO1985 <ordicu85@gmail.com>
2022-02-22 00:55:45 +0000

    Undoing changes

--- frontend/src/app/core/layout/sidebar/sidebar.component.ts
+++ frontend/src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,9 +1,7 @@
-import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from "@angular/core";
 import { Observable } from 'rxjs';
 import { BeersService } from 'src/app/pages/beers/beers.service';
 import { Beer } from 'src/app/pages/beers/models/beer';
-import { UserService } from '../../services/user.service';
 
 @Component({
   selector: "app-sidebar",
@@ -12,15 +10,12 @@ import { UserService } from '../../services/user.service';
 })
 export class SidebarComponent implements OnInit {
   beers$!: Observable<Beer[]>;
-  isAuthenticated$!: Observable<boolean>;
 
   constructor(
     private beerService: BeersService,
-    private userService: UserService,
   ) {}
 
   ngOnInit(): void {
-    this.isAuthenticated$ = this.userService.isAuthenticated$;
     this.beers$ = this.beerService.getTopTenBeers();
   }
 }
