5a5961437189c71e3f7bbaad42fc5e47eb55ced3
JoseO1985 <ordicu85@gmail.com>
2022-02-22 00:52:29 +0000

    Adding beer details component

--- frontend/src/app/pages/beers/beers-details/beer-details.component.ts
+++ frontend/src/app/pages/beers/beers-details/beer-details.component.ts
@@ -1,6 +1,10 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable, Subscription } from 'rxjs';
+import { BeersService } from '../beers.service';
+import { Beer } from '../models/beer';
 
 @Component({
   selector: 'app-beer-details',
@@ -10,16 +14,28 @@ import { Subscription } from 'rxjs';
 export class BeerDetailsComponent implements OnInit, OnDestroy {
 
   beerId = "";
+  beer$!: Observable<Beer>;
   subscription!: Subscription;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router,
+    private beerService: BeersService,
+    private toastrService: ToastrService
   ) { }
 
   ngOnInit(): void {
     this.subscription = this.route.paramMap.subscribe(params => {
       this.beerId= params.get('id') || "";
-      console.log(this.beerId);
+      if (!this.beerId) {
+        this.toastrService.error('Missing parameter');
+        this.router.navigate(['beers']);
+        return;
+      }
+      this.beer$ = this.beerService.getById(this.beerId);
+    },
+    error => {
+      this.toastrService.error(error.message);
     });
   }
 
