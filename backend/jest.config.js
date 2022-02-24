1b1999adefdd0934403d4bcf7835936e560a36b7
JoseO1985 <ordicu85@gmail.com>
2022-02-24 00:31:06 +0000

    Adding jest config

---
+++ backend/jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: [
+    "js",
+    "jsx",
+    "json",
+    "node",
+    "ts"
+  ]
+};
